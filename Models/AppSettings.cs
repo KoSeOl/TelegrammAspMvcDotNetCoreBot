d81c6603de31e421e820e91f884930dd0fec04de
Sergey <koseol1984@gmail.com>
2021-08-11 15:23:07 +0300

    11/08/21

---
+++ Models/AppSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TelegrammAspMvcDotNetCoreBot.Models
+{
+    public static class AppSettings
+    {
+        public static string Url { get; set; } = "https://URL:443/{0}";
+        public static string Name { get; set; } = "PEZ_Support_Bot";
+        public static string Key { get; set; } = "896669328:AAGXUiRyRBMW2G3oPUggqxDZXbQpb23T5QM";
+    }
+}
